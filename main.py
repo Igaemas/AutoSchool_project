4e73a116b13a81a63ebeaffb9f97657fff9ef67d
Igaemas <sasha.deroubaix@icloud.com>
2022-01-07 11:58:59 +0100

    mise a jour, optimisation avec des fonctions

--- main.py
+++ main.py
@@ -2,25 +2,54 @@ import os
 
 # -- user configuration --
 projectPath = ""
-
 userName = os.getlogin()
 userPath = "c:/Users/" + userName + "/Documents/AutoCode/"
 
-# -- user choice --
-print("sur quoi voulez-vous travailler ? : ")
-print("1 -> HTML")
-userChoice = int(input("Que choisissez-vous ? :"))
+def Start_Choice():
+  print("que voulez vous faire ? : ")
+  print("1 -> executer un cours\n2 -> configurer le logiciel")
+  userChoice = int(input("Que choisissez-vous ? :"))
+  match userChoice:
+    case 1:
+      Start_Code()
+    case 2:
+      File_Configuration()
 
-match userChoice:
-  case 1:
-    projectPath = userPath + "html/"
-  case 2:
-    projectPath = userPath + "python/"
+def File_Configuration():
+  if os.path.exists(userPath) == False :
+    os.mkdir(userPath) 
+  if os.path.exists(userPath + "config/") == False :
+    os.mkdir(userPath + "config/")
+    localConfig = open(userPath + "config/file_config.txt", "w+")
+    localConfig.write("0")
+    localConfig.close
+  print("  \nFichier de configuration créé\n  ")
+  Start_Choice()
+
+def Start_Code():
+  
+  print("sur quoi voulez-vous travailler ? : ")
+  print("1 -> HTML")
+  userChoice = int(input("Que choisissez-vous ? :"))
+
+  match userChoice:
+    case 1:
+      projectPath = userPath + "html/"
+      print(projectPath)
+    case 2:
+      projectPath = userPath + "python/"
+
+# ----------------------------------------------------------------
+
+Start_Choice()
 
 # -- dir project creation --
 if os.path.exists(projectPath) == True :
-  print("this directory already exist.")
-  
+  print("this directory already exist.")  
 else :
-  print(projectPath)
+  localConfig = open(userPath + "config/file_config.txt", "w+")
+  filename = localConfig.read() + ".html"
+  localConfig.write("1")
+  localConfig.close
+  os.mkdir(projectPath)
   
